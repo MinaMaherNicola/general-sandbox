f2d416bb612e03610bdfc1cf6d3f21b3ffc90d5d
MinaMaherNicola <minamahernicola@outlook.com>
2023-02-05 18:20:14 +0200

    finished section 4 in spring

---
+++ LearningSpring/src/springdemo/SpringHelloApp.java
@@ -0,0 +1,14 @@
+package springdemo;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringHelloApp {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach coach = context.getBean("coach", Coach.class);
+
+        System.out.println(coach.getDailyWorkout());
+
+        context.close();
+    }
+}
