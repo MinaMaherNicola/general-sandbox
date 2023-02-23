f35479de4873e8e00498ddc0c6d8791509d3606e
MinaMaherNicola <minamahernicola@outlook.com>
2023-02-23 19:21:19 +0200

    learned about Spring Annotations inversion of control

--- LearningSpring/SpringAnnotationConfigs/src/Main.java
+++ LearningSpring/SpringAnnotationConfigs/src/Main.java
@@ -1,5 +1,11 @@
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springannotations.Coach;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach coach = context.getBean("tennisCoach", Coach.class);
+        System.out.println(coach.getDailyWorkout());
+        context.close();
     }
 }
---
+++ LearningSpring/SpringAnnotationConfigs/src/springannotations/TennisCoach.java
@@ -0,0 +1,12 @@
+package springannotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "Tennis workout.";
+    }
+}
