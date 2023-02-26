381a58e303c98e444e9eeb19ea992c11a6d225aa
MinaMaherNicola <minamahernicola@outlook.com>
2023-02-26 21:39:49 +0200

    learned about annotation beans scopes and lifecycles methods

--- LearningSpring/SpringAnnotationConfigs/src/springannotations/TennisCoach.java
+++ LearningSpring/SpringAnnotationConfigs/src/springannotations/TennisCoach.java
@@ -3,13 +3,15 @@ package springannotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
     @Value("${email}")
     private String email;
-    @Qualifier("randomFortuneService")
+
     private final FortuneService fortuneService;
 
     @Autowired
