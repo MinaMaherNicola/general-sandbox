4a6769f9480cd936ef4b27cc46f9a7a74176cac4
MinaMaherNicola <minamahernicola@outlook.com>
2023-03-10 00:10:32 +0200

    solo practice project init

---
+++ jvm-based-languages/LearningSpring/practice/src/test/java/solo/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package solo.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
