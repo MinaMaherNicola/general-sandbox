aab50a2a17dfe2ce94d63c9a8fc39ca8f9e2c04a
MinaMaherNicola <minamahernicola@outlook.com>
2023-03-17 14:20:19 +0200

    finished chapter 4 and learned about logging

---
+++ dotnet-based-languages/CSharp/cs11dotnet7/Chapter04/Chapter04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Chapter04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string logPath = @"E:\code\languages-sandbox\dotnet-based-languages\CSharp\cs11dotnet7\Chapter04\logs.txt";
+
+            TextWriterTraceListener logFile = new(File.Create(logPath, 0, FileOptions.WriteThrough));
+            Trace.Listeners.Add(logFile);
+
+            Trace.AutoFlush = true;
+            Debug.WriteLine("Debug is watching");
+            Trace.WriteLine("Trace is watching");
+
+            int unitInStocks = 12;
+            LogSourceDetails(unitInStocks > 10);
+        }
+
+        static void LogSourceDetails(bool condition,
+                                     [CallerMemberName] string member = "",
+                                     [CallerFilePath] string filePath = "",
+                                     [CallerLineNumber] int line = 0,
+                                     [CallerArgumentExpression(nameof(condition))] string expression = "")
+        {
+            Trace.WriteLine($"{filePath}\n{member} on line {line}.\nExpression: {expression}");
+        }
+    }
+}
