390642a8e6dd2cb238eb3dd2018bcc56def11384
MinaMaherNicola <minamahernicola@outlook.com>
2023-05-21 19:32:24 +0300

    edited some .gitignore rules

--- assembly/ch03/myexit.s
+++
@@ -1,8 +0,0 @@
-# this is a comment in assembly
-.global _start
-.section .text
-_start:
-	movq $60, %rax
-	movq $3, %rdi
-	syscall
-
--- assembly/ch04/binary.s
+++
@@ -1,6 +0,0 @@
-.globl _start
-.section .text
-_start:
-	movq $0b1101, %rdi # move this binary value into %rdi
-	movq $60, %rax # move the end program call into rax
-	syscall
