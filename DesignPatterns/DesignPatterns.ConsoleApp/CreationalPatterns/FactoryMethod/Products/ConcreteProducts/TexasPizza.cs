f1c5bfcf61b36f1a13e4097950b92752d283c5a2
MinaMaherNicola <minamahernicola@outlook.com>
2023-11-29 14:27:12 +0200

    studied factory method design pattern

---
+++ DesignPatterns/DesignPatterns.ConsoleApp/CreationalPatterns/FactoryMethod/Products/ConcreteProducts/TexasPizza.cs
@@ -0,0 +1,12 @@
+﻿using DesignPatterns.ConsoleApp.CreationalPatterns.FactoryMethod.Products.ProductContract;
+
+namespace DesignPatterns.ConsoleApp.CreationalPatterns.FactoryMethod.Products.ConcreteProducts
+{
+    public class TexasPizza : IPizza
+    {
+        public void EatPizza()
+        {
+            Console.WriteLine("Eating Texas Pizza");
+        }
+    }
+}
