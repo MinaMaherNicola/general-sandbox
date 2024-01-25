711e7c551bd217e2ea5a2b947c045422becfd683
MinaMaherNicola <minamahernicola@outlook.com>
2024-01-25 15:28:17 +0200

    removed go to relearn

--- go/learning-go/ch-05/defer.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	// In Go, the defer keyword is used to schedule a function call to be executed after the surrounding function returns. 
-	defer fmt.Println("World!")
-	fmt.Print("Hello ")
-}
