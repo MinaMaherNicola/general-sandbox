029ce7b085a3f88dae8e869b1e50ac9a4fec502e
MinaMaherNicola <minamahernicola@outlook.com>
2024-04-10 19:43:33 +0200

    learned about exchanges and binding

---
+++ dotnet/general/rabbitmq/PubSub/Publisher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        
+        channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+
+        for (int i = 0; i < 10; i++)
+        {
+            var message = $"log message {i}";
+            channel.BasicPublish(exchange: "logs",
+                                 routingKey: string.Empty,
+                                 basicProperties: null,
+                                 body: Encoding.UTF8.GetBytes(message));
+        }
+        Console.WriteLine("Publisher finished sending logs");
+    }
+}
---
+++ dotnet/general/rabbitmq/PubSub/Subscriber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Subscriber;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        
+        channel.ExchangeDeclare("logs", ExchangeType.Fanout);
+        var queueName = channel.QueueDeclare().QueueName;
+        channel.QueueBind(queueName, exchange: "logs", routingKey: string.Empty);
+        Console.WriteLine("Waiting for logs");
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (_, eventArgs) =>
+        {
+            var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+            Console.WriteLine($"Subscriber received log: {message}");
+        };
+
+        channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+        Console.WriteLine("Press any key to exit");
+        Console.ReadKey();
+    }
+}
