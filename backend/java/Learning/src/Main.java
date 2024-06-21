c22f05efe7f5faa45a1434ded5270e5c983dd64d
MinaMaherNicola <minamahernicola@outlook.com>
2024-06-21 22:01:39 +0300

    changed some stuff

--- backend/java/Learning/src/Main.java
+++ backend/java/Learning/src/Main.java
@@ -1,11 +1,8 @@
-import Services.Users.ProblemSolver;
+import Problems.Solver;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ProblemSolver solver = new ProblemSolver();
-
-        System.out.println(Arrays.toString(solver.twoSum(new int[]{3, 3}, 6)));
     }
 }
---
+++ backend/java/Learning/src/Problems/Solver.java
@@ -0,0 +1,6 @@
+package Problems;
+
+import java.util.HashMap;
+
+public class Solver {
+}
