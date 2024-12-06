3d343359b67c389a56ca9a09f39b5e46c2d54b5c
MinaMaherNicola <minamahernicola@outlook.com>
2024-12-06 18:04:51 +0200

    learned the basics of rabbitmq

---
+++ tools/rabbitmq/DotnetRabbitMqPractice/Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer;
+
+using QueueBuilder;
+
+internal abstract class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await using MqBuilder builder = new();
+
+        var channel = builder.GetChannel();
+        
+        await channel.QueueDeclareAsync(queue: "hello", false, false, false, arguments: null);
+
+        
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        consumer.ReceivedAsync += async (sender, @event) =>
+        {
+            string message = Encoding.UTF8.GetString(@event.Body.ToArray());
+
+            Console.WriteLine($"Consumer 1 received {message}");
+
+            await Task.Delay(2000);
+
+            await channel.BasicAckAsync(@event.DeliveryTag, false);
+        };
+
+        await channel.BasicConsumeAsync("hello", autoAck: false, consumer: consumer);
+        
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
---
+++ tools/rabbitmq/DotnetRabbitMqPractice/Producer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using QueueBuilder;
+using RabbitMQ.Client;
+
+namespace Producer;
+
+internal abstract class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await using MqBuilder builder = new();
+
+        var channel = builder.GetChannel();
+
+        await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+            arguments: null);
+        
+        await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("1. Press 'c' to exit");
+                Console.WriteLine("2. Press 's' to send message.");
+                var key = Console.ReadKey();
+
+                if (key.Key == ConsoleKey.S)
+                {
+                    await SendMessage(channel, new Random().Next(100, 1000).ToString());
+                }
+                else if (key.Key == ConsoleKey.C)
+                {
+                    break;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("\nPlease, only use valid keys\n");
+            }
+
+            
+        }
+    }
+
+    private static async Task SendMessage(IChannel channel, string message)
+    {
+        if (string.IsNullOrWhiteSpace(message))
+        {
+            return;
+        }
+        var messageInBytes = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: messageInBytes);
+        Console.WriteLine("\nProducer sent one message\n");
+    }
+}
