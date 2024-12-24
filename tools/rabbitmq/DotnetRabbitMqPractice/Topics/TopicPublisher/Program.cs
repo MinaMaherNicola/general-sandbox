8415dbaaab581c2aa9b03bfd219f5275da6d949c
MinaMaherNicola <minamahernicola@outlook.com>
2024-12-25 01:26:26 +0200

    learned about rabbitmq topics

---
+++ tools/rabbitmq/DotnetRabbitMqPractice/Topics/TopicPublisher/Program.cs
@@ -0,0 +1,18 @@
+﻿using QueueBuilder;
+using RabbitMQ.Client;
+
+namespace TopicPublisher;
+
+internal abstract class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await using var mq = new RabbitMq();
+
+       await using var channel = mq.GetChannel();
+
+        await channel.ExchangeDeclareAsync("topics", ExchangeType.Topic, true, true);
+
+        await mq.EnterSendMode("topics", string.Empty, "general.info");
+    }
+}
---
+++ tools/rabbitmq/DotnetRabbitMqPractice/Topics/TopicsGeneralConsumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using QueueBuilder;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace TopicsGeneralConsumer;
+
+internal abstract class Program
+{
+    private static async Task Main(string[] args)
+    {
+        await using var mq = new RabbitMq();
+
+        var channel = mq.GetChannel();
+        
+        await channel.ExchangeDeclareAsync("topics", ExchangeType.Topic, true, true);
+
+        var queue = await channel.QueueDeclareAsync();
+
+        await channel.BasicQosAsync(0, 1, false);
+
+        await channel.QueueBindAsync(queue.QueueName, "topics", "general.#");
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        consumer.ReceivedAsync += async (_, ea) =>
+        {
+            var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+            Console.WriteLine($"General Consumer: {message}");
+
+            await channel.BasicAckAsync(ea.DeliveryTag, false);
+        };
+
+        await channel.BasicConsumeAsync(queue.QueueName, false, consumer);
+
+        Console.ReadLine();
+    }
+}
